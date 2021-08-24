117fb354f553bef844b8bfd1562c4d08d104a5bb
Rodrigo <rodrigo.prestes@gmail.dom>
2021-08-24 16:27:09 -0300

    remoção do exemplo angular-crud do branch de dev

--- CrudWS/wildfly.sh
+++
@@ -1,8 +0,0 @@
-#! /bin/bash 
-
-JBOSS_HOME=/opt/jboss/wildfly
-JBOSS_CLI=$JBOSS_HOME/bin/jboss-cli.sh
-
-$JBOSS_HOME/bin/standalone.sh -b 0.0.0.0 -bmanagement 0.0.0.0
-#sleep 10
-#$JBOSS_CLI --connect --user=pw2 --password=pw2 --file=$JBOSS_HOME/bin/wildfly.cli
