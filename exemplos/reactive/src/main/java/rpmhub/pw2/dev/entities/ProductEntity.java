5249312ebf861b1c51e1a572febe54efaaf5f114
Rodrigo Prestes Machado <rodrigo.prestes@gmail.com>
2023-05-23 13:38:48 -0300

    clean reactive

---
+++ exemplos/reactive/src/main/java/rpmhub/pw2/dev/entities/ProductEntity.java
@@ -0,0 +1,28 @@
+/**
+ * PW2 by Rodrigo Prestes Machado
+ *
+ * PW2 is licensed under a Creative Commons Attribution 4.0 International
+ * License. You should have received a copy of the license along with this
+ * work. If not, see <http://creativecommons.org/licenses/by/4.0/>.
+ */
+package rpmhub.pw2.dev.entities;
+
+import io.quarkus.hibernate.reactive.panache.PanacheEntityBase;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.Id;
+import lombok.Getter;
+import lombok.Setter;
+
+@Entity
+@Getter @Setter
+public class ProductEntity extends PanacheEntityBase  {
+
+    @Id
+    @GeneratedValue
+    private Long id;
+    private String sku;
+    private String name;
+    private String description;
+
+}
